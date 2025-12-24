5b5dfcdcaab0c14de69a88ff8c7b1512568083ae
Abirullah <abirafridi87@gmail.com>
2025-12-24 08:00:13 +0500

    completed work page updated

---
+++ src/Components/Loder.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Components.css'
+
+
+export default function Loder() {
+  return (
+    <div className="flex absolute bg-none  items-center justify-center">
+      <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
+    </div>
+  );
+}
+
